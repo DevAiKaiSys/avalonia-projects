21ea8343303b11c375297b3892ab669f626e3ca8
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-03-09 19:55:02 +0700

    18. Sub-View Model Binding

--- BatchProcess3/ViewModels/ActionsPageViewModel.cs
+++ BatchProcess3/ViewModels/ActionsPageViewModel.cs
@@ -1,6 +1,7 @@
 using BatchProcess3.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -18,6 +19,9 @@ public partial class ActionsPageViewModel() : PageViewModel(ApplicationPageNames
     [ObservableProperty]
     private ObservableCollection<ActionsPrintViewModel> _printList;
 
+    [ObservableProperty]
+    private ActionsPrintViewModel _selectedPrintListItem;
+
     [RelayCommand]
     public void RefreshActionsPage(ActionsPageName actionsPageName)
     {
@@ -33,9 +37,25 @@ public partial class ActionsPageViewModel() : PageViewModel(ApplicationPageNames
         // TODO: Fetch from a database/service provider
         PrintList =
         [
-            new ActionsPrintViewModel { Id = "1", JobName = "Print Only Drawings" },
-            new ActionsPrintViewModel { Id = "2", JobName = "Print All Drawings Scale To Fit" },
-            new ActionsPrintViewModel { Id = "3", JobName = "Print 3D Models A3" }
+            new ActionsPrintViewModel
+            {
+                Id = "1",
+                JobName = "Print Only Drawings",
+                Description = "Prints only drawing files",
+                PrintDrawingRange = "0, 5, 7-8",
+                PrintDrawings = true,
+                DrawingExclusionList = $"Some item 1{Environment.NewLine}Some item 2{Environment.NewLine}Some item 3"
+            },
+            new ActionsPrintViewModel
+            {
+                Id = "2", JobName = "Print All Drawings Scale To Fit",
+                Description = "Prints drawing scaled to fit the paper", PrintDrawings = true
+            },
+            new ActionsPrintViewModel
+            {
+                Id = "3", JobName = "Print 3D Models A3", Description = "Prints models as 3D visuals",
+                PrintModels = true
+            }
         ];
     }
 
--- BatchProcess3/ViewModels/ActionsPrintViewModel.cs
+++ BatchProcess3/ViewModels/ActionsPrintViewModel.cs
@@ -5,11 +5,32 @@ namespace BatchProcess3.ViewModels;
 public partial class ActionsPrintViewModel : ViewModelBase
 {
     [ObservableProperty]
+    private string _description;
+
+    [ObservableProperty]
+    private string _drawingExclusionList;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(DrawingExclusionListTitle))]
+    private bool _drawingExlusionIsWhiteList;
+
+    [ObservableProperty]
     private string _id;
 
     [ObservableProperty]
-    private string _jobName;
-    
-    [ObservableProperty]
     private bool _isSelected;
+
+    [ObservableProperty]
+    private string _jobName;
+
+    [ObservableProperty]
+    private string _printDrawingRange;
+
+    [ObservableProperty]
+    private bool _printDrawings;
+
+    [ObservableProperty]
+    private bool _printModels;
+
+    public string DrawingExclusionListTitle => DrawingExlusionIsWhiteList ? "White List" : "Black List";
 }
