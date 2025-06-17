9177ba8581299acf06dce689b52c2299e008f75a
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-06-17 22:33:47 +0700

    Animated popup 009. Avalonia UI - Animated Popups Custom Overlays

--- AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
+++ AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AvaloniaLoudnessMeter.ViewModels;
 
@@ -6,7 +7,16 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _boldTitle = "AVALONIA";
-    
+
+    [ObservableProperty]
+    private bool _channelConfigurationListIsOpen;
+
     [ObservableProperty]
     private string _regularTitle = "LOUDNESS METER";
+
+    [RelayCommand]
+    private void ChannelConfigurationButtonPressed()
+    {
+        ChannelConfigurationListIsOpen ^= true;
+    }
 }
--- AvaloniaLoudnessMeter/Views/MainView.axaml.cs
+++ AvaloniaLoudnessMeter/Views/MainView.axaml.cs
@@ -1,11 +1,64 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Threading;
+using AvaloniaLoudnessMeter.ViewModels;
 
 namespace AvaloniaLoudnessMeter.Views;
 
 public partial class MainView : UserControl
 {
+    #region Constructor
+
+    /// <summary>
+    ///     Add commentMore actions
+    ///     Default constructor
+    /// </summary>
+    /// <exception cref="Exception">Throws if named controls cannot be found</exception>
     public MainView()
     {
         InitializeComponent();
+
+        // Gather the named controls
+        _channelConfigButton = this.FindControl<Control>("ChannelConfigurationButton") ??
+                               throw new Exception("Cannot find Channel Configuration Button by name");
+        _channelConfigPopup = this.FindControl<Control>("ChannelConfigurationPopup") ??
+                              throw new Exception("Cannot find Channel Configuration Popup by name");
+        _mainGrid = this.FindControl<Control>("MainGrid") ?? throw new Exception("Cannot find Main Grid by name");
     }
+
+    #endregion
+
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+
+        var position = _channelConfigButton.TranslatePoint(new Point(), _mainGrid) ??
+                       throw new Exception("Cannot get TranslatePoint");
+
+        Dispatcher.UIThread.Post(() =>
+        {
+            _channelConfigPopup.Margin = new Thickness(
+                position.X,
+                0,
+                0,
+                _mainGrid.Bounds.Height - position.Y - _channelConfigButton.Bounds.Height);
+        });
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        ((MainViewModel)DataContext!).ChannelConfigurationButtonPressedCommand.Execute(null);
+    }
+
+
+    #region Private Members
+
+    private readonly Control _channelConfigPopup;
+    private readonly Control _channelConfigButton;
+    private readonly Control _mainGrid;
+
+    #endregion
 }
