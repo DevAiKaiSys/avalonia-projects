d1fa5c7cfbaccb14f3bea7856c45c968de8927bf
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-09-28 00:10:28 +0700

    Process Page initial work 51. Process List Page

--- BatchProcess3/ViewModels/ProcessPageViewModel.cs
+++ BatchProcess3/ViewModels/ProcessPageViewModel.cs
@@ -1,55 +1,18 @@
-
-using BatchProcess3.DataStorage;
-using BatchProcess3.MainApp;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using BatchProcess3.DataStorage;
+using BatchProcess3.Dialog;
+using BatchProcess3.MainApp;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BatchProcess3.ViewModels;
 
 public partial class ProcessPageViewModel : PageViewModel
 {
-    #region Members
-
-    private DatabaseService _databaseService;
-        
-    #endregion
-    
-    #region Properties
-
-    [ObservableProperty] private ObservableCollection<ProcessViewModel> _processList = [];
-
-    public bool ProcessListHasItems => ProcessList.Any();
-
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(SelectedProcessListItem))]
-    private string _selectedProcessListItemId = "";
-
-    public ProcessViewModel? SelectedProcessListItem =>
-        ProcessList.FirstOrDefault(f => f.Id == SelectedProcessListItemId);
-
-    #endregion
-
-    #region Constructor
-
-    public ProcessPageViewModel(DatabaseService databaseService) : base(ApplicationPageNames.Process)
-    {
-        _databaseService = databaseService;
-        
-        FetchProcesses();
-    }
-
-// Design-time only
-    public ProcessPageViewModel() : this(new DatabaseService(new ApplicationDbContext()))
-    {
-        if (!Avalonia.Controls.Design.IsDesignMode) throw new InvalidOperationException("Parameterless constructor is only for design time use");
-    }
-
-    protected override void OnDesignTimeConstructor() => FetchProcesses();
-    
-    #endregion
-    
     [RelayCommand]
     private void FetchProcesses()
     {
@@ -72,4 +35,182 @@ public partial class ProcessPageViewModel : PageViewModel
             item.SetSavedState();
     }
 
+    [RelayCommand]
+    private void AddNewProcess()
+    {
+        // Create a new item
+        var newItem = new ProcessViewModel
+        {
+            Id = Guid.NewGuid().ToString("N"),
+            IsNewItem = true,
+            JobName = "New Process"
+        };
+
+        // Add to the print list
+        ProcessList.Add(newItem);
+
+        // Select item
+        SelectedProcessListItemId = newItem.Id;
+    }
+
+    [RelayCommand]
+    private async Task DeleteProcessItemAsync(string id)
+    {
+        if (ProcessList.Count(x => x.Id == id) != 1)
+            // TODO: Throw/Warn?
+            return;
+
+        // If user selected to remove from UI (via Confirm dialog)
+        if (await DeleteProcessItemFromUIAsync(id))
+            // Delete from database
+            _databaseService.DeleteProcessItem(id);
+    }
+
+    // ReSharper disable once InconsistentNaming
+    private async Task<bool> DeleteProcessItemFromUIAsync(string id, bool warn = true)
+    {
+        var index = ProcessList.IndexOf(ProcessList.First(x => x.Id == id));
+        if (index == -1)
+            return false;
+
+        if (warn)
+        {
+            var confirmViewModel = new ConfirmDialogViewModel
+            {
+                Title = "Delete Process Item?",
+                Message = $"Are you sure you want to delete ' {ProcessList[index].JobName}'?",
+                DialogWidth = 500
+                // OnConfirm = async (vm) =>
+                // {
+                //     await Task.Delay(2000);
+                //
+                //     vm.ProgressText = "This is taking a while...";
+                //
+                //     await Task.Delay(2000);
+                //     
+                //     vm.StatusText = "Oh no, something went wrong...";
+                //
+                //     return true;
+                // }
+            };
+
+            await _dialogService.ShowDialog(_mainViewModel, confirmViewModel);
+
+            // Ignore if we clicked cancel
+            if (!confirmViewModel.Confirmed)
+                return false;
+        }
+
+        // Remove item
+        ProcessList.RemoveAt(index);
+
+        // Select the item below the deleted one
+        if (index > 0) index--;
+
+        if (ProcessList.Count > 0)
+            SelectedProcessListItemId = ProcessList[index].Id;
+
+        return true;
+    }
+
+    [RelayCommand]
+    private void DeleteSelectedProcessAction(int sortOrder)
+    {
+        if (SelectedProcessListItem == null)
+            // TODO: Throw/Warn?
+            return;
+
+        var item = SelectedProcessListItem.Actions.FirstOrDefault(f => f.SortOrder == sortOrder);
+
+        if (item != null)
+            SelectedProcessListItem.Actions.Remove(item);
+    }
+
+    [RelayCommand]
+    private async Task CancelProcessItem()
+    {
+        // Ignore if nothing is selected
+        if (SelectedProcessListItem == null)
+            return;
+
+        // If the selected item is new, delete it
+        // Otherwise, restore from save state
+        if (SelectedProcessListItem.IsNewItem)
+            await DeleteProcessItemFromUIAsync(SelectedProcessListItem.Id, false);
+        else
+            SelectedProcessListItem.RestoreState();
+    }
+
+    [RelayCommand]
+    private Task SaveProcessItemAsync()
+    {
+        // Ignore if no selection
+        if (SelectedProcessListItem == null)
+            return Task.CompletedTask;
+
+        // If the selected item is new...
+        if (SelectedProcessListItem.IsNewItem)
+            _databaseService.AddProcessItem(SelectedProcessListItem.ToDataModel());
+        else
+            _databaseService.UpdateProcessItem(SelectedProcessListItem.ToDataModel());
+
+        // Flag new item as not new
+        SelectedProcessListItem.IsNewItem = false;
+        SelectedProcessListItem.SetSavedState();
+
+        return Task.CompletedTask;
+    }
+
+    #region Members
+
+    private readonly DatabaseService _databaseService;
+    private readonly MainViewModel _mainViewModel;
+    private readonly DialogService _dialogService;
+
+    #endregion
+
+    #region Properties
+
+    [ObservableProperty]
+    private ObservableCollection<ProcessViewModel> _processList = [];
+
+    public bool ProcessListHasItems => ProcessList.Any();
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(SelectedProcessListItem))]
+    private string _selectedProcessListItemId = "";
+
+    public ProcessViewModel? SelectedProcessListItem =>
+        ProcessList.FirstOrDefault(f => f.Id == SelectedProcessListItemId);
+
+    #endregion
+
+    #region Constructor
+
+    public ProcessPageViewModel(
+        MainViewModel mainViewModel,
+        DialogService dialogService,
+        DatabaseService databaseService) : base(ApplicationPageNames.Process)
+    {
+        _mainViewModel = mainViewModel;
+        _dialogService = dialogService;
+        _databaseService = databaseService;
+
+        FetchProcesses();
+    }
+
+// Design-time only
+    public ProcessPageViewModel() : this(new MainViewModel(), new DialogService(() => null),
+        new DatabaseService(new ApplicationDbContext()))
+    {
+        if (!Design.IsDesignMode)
+            throw new InvalidOperationException("Parameterless constructor is only for design time use");
+    }
+
+    protected override void OnDesignTimeConstructor()
+    {
+        FetchProcesses();
+    }
+
+    #endregion
 }
--- BatchProcess3/Views/ProcessPageView.axaml.cs
+++ BatchProcess3/Views/ProcessPageView.axaml.cs
@@ -1,6 +1,5 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using BatchProcess3.ViewModels;
 
 namespace BatchProcess3.Views;
 
@@ -9,5 +8,16 @@ public partial class ProcessPageView : UserControl
     public ProcessPageView()
     {
         InitializeComponent();
+    }
+
+    private void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems?.Count > 0 && e.AddedItems[0] is ProcessViewModel viewModel)
+            // When it is a newly created item
+            if (viewModel.IsNewItem)
+            {
+                JobNameTextBox.SelectAll();
+                JobNameTextBox.Focus();
+            }
     }
 }
